c2760108da117f74f683d94674976378408ea4fb
vsirising <83484400+vsirising@users.noreply.github.com>
2021-05-02 00:08:03 -0400

    Delete deletecontent.php

--- deletecontent.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-$id = $_GET['id'];
-$sql = 'DELETE FROM contents WHERE ContentID=:id';
-$statement = $db->prepare($sql);
-if ($statement->execute([':id' => $id])) {
-  header("Location: editcontent.php");
-}
