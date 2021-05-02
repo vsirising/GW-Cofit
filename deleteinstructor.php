5f000a473be3f690fee7130c9921661d709c3b56
vsirising <83484400+vsirising@users.noreply.github.com>
2021-05-02 00:08:06 -0400

    Delete deleteinstructor.php

--- deleteinstructor.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-$id = $_GET['id'];
-$sql = 'DELETE FROM account WHERE AccountID=:id';
-$statement = $db->prepare($sql);
-if ($statement->execute([':id' => $id])) {
-  header("Location: editinstructor.php");
-}
